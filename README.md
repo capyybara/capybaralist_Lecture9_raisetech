58992dc61a137baa35e326ecdebe14fd53cb94dd
capybara <sakibara.ex.9175@gmail.com>
2023-10-04 18:53:07 +0900

    zoosテーブル＿READ機能の実装 確認用

---
+++ README.md
@@ -0,0 +1,24 @@
+# MyBatis で CRUD の実装
+
+## 概要
+・カピバラが飼育されている施設（動物園）および、そこで飼育されているカピバラの名前（命名されているものを一部抜粋）に関するDB　<br>
+・zoosテーブルに関するRead処理　<br>
+・capybaraテーブルのzoo_idのデータ取得＆例外処理（をトライしようとしていますが、詰まりまくっています😅）
+<br><br>
+
+
+## 動作確認
+
+### curlコマンド（GET）
+- zoosテーブル
+
+<img width="1440" alt="curl zoo_id=4まで" src="https://github.com/capyybara/RaiseTech_Task4_Stream/assets/137416338/0c4a1491-8025-4ac0-938a-0875e676108d">
+
+<br>
+
+### Postman（GET）
+- zoosテーブル
+
+<img width="1440" alt="postman zoo_id=4" src="https://github.com/capyybara/RaiseTech_Task4_Stream/assets/137416338/a5219998-8dd4-4a48-ab6e-0803144cb7a6">
+
+<br><br>
--- src/main/java/com/Task9/capybaralist/Service/CapybaraServiceImpl.java
+++ src/main/java/com/Task9/capybaralist/Service/CapybaraServiceImpl.java
@@ -1,10 +1,12 @@
 package com.Task9.capybaralist.Service;
 
+
 import com.Task9.capybaralist.mapper.CapybaraMapper;
 import com.Task9.capybaralist.entity.Zoo;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 @Service
 public class CapybaraServiceImpl implements CapybaraService{
@@ -18,5 +20,17 @@ public class CapybaraServiceImpl implements CapybaraService{
     public List<Zoo> findAll() {
         return capybaraMapper.findAll();
     }
+
+
+//  カピバラテーブルから、例外をハンドリングしてデータ取得したい
+//    @Override
+//    public Optional<Capybara> findById(int zoo_id) throws Exception {
+//        Optional<Capybara> capybara = capybaraMapper.findById(zoo_id);
+//        if capybara isEmpty(){
+//            throw new NotFoundException("見つかりません");
+//        }
+//    }
+
+
 
 }
